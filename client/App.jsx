0e7d887a1819c2b1d3336e4e43b765d4b9fd0ebc
7u1ian <jsn.chn@outlook.com>
2021-08-30 06:23:48 -0700

    Refactor app to functional component

--- client/App.jsx
+++ client/App.jsx
@@ -10,50 +10,39 @@ import {
 import LandingPage from './LandingPage/LandingPage.jsx';
 import NavBar from './shared/NavBar.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+function App() {
 
-  render() {
-    return (
-      <>
-        <div className="landing-page-main-div">
-          <Router>
-            <NavBar />
+  return (
+    <>
+      <div className="landing-page-main-div">
+        <Router>
+          <NavBar />
 
-            <Route exact path="">
-              <LandingPage />
-            </Route>
+          <Route exact path="">
+            <LandingPage />
+          </Route>
 
-            <Route path="/sign-in">
-              {/* <SignIn /> */}
-              <h2>Hello Sign in</h2>
-            </Route>
+          <Route path="/sign-in">
+            {/* <SignIn /> */}
+            <h2>Hello Sign in</h2>
+          </Route>
 
-            <Route path="/search">
-              <h2>Hello Search</h2>
-            </Route>
+          <Route path="/search">
+            <h2>Hello Search</h2>
+          </Route>
 
-            <Route path="/cart">
-              <h2>Hello Shopping Cart</h2>
-            </Route>
+          <Route path="/cart">
+            <h2>Hello Shopping Cart</h2>
+          </Route>
 
-            <Route path="/checkout">
-              <h2>Hello Checkout</h2>
-            </Route>
-          </Router>
-        </div>
-      </>
-    );
-  }
+          <Route path="/checkout">
+            <h2>Hello Checkout</h2>
+          </Route>
+        </Router>
+      </div>
+    </>
+  );
 
 }
-
-{ /* <h1>Hello World</h1> */ }
-
-
-
 
 ReactDOM.render(<App />, document.getElementById('app'));
