831a2146b721c328855171af2df24219ff7b2dcf
Erin Grisham <erinmgrisham@gmail.com>
2021-08-30 12:15:42 -0400

    skeleton of navbar complete; fine tuning per wireframes next

--- client/shared/NavBar.jsx
+++ client/shared/NavBar.jsx
@@ -1,35 +1,220 @@
 import React from 'react';
-import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { Link } from 'react-router-dom';
 
-class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
 
-  render() {
-    return (
-      <nav className="landing-page-nav-bar">
-        <div className="landing-page-logo">
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import useStyles from './styles.js';
 
-        </div>
-        <div className="landing-page-search-bar-div">
-          <input className="landing-page-search-bar-textbox" type="text" src="https://www.kindacode.com/wp-content/uploads/2020/12/search.png" placeholder="Search"></input>
-        </div>
-        <div className="landing-page-location-input-div">
-          <input className="landing-page-location-input-textbox" type="text" placeholder="New York City"></input>
-        </div>
-        <div className="landing-page-login-link-div">
-          <Link to="/sign-in">Sign in</Link>
-        </div>
-        <div className="landing-page-cart-link-div">
-          <Link to="/cart"><ShoppingCartOutlinedIcon /></Link>
+const NavBar = () => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-        </div>
-      </nav>
-    );
-  }
-}
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+
+    <div className={classes.grow}>
+      <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none', color: 'black'}}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Odds 'N Ends
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <LocationOnIcon />
+            </div>
+            <InputBase
+              placeholder="Location…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+              style={{marginRight: '10px'}}
+            >
+              <div style={{marginRight: '10px'}}>
+                <Typography variant="h6" noWrap>
+                  <Link to="/sign-in">Sign in</Link>
+                </Typography>
+              </div>
+            </IconButton>
+            <IconButton aria-label="show new notifications" color="inherit">
+              <Badge badgeContent={1} color="secondary">
+                <Link to="/cart"><ShoppingCartIcon /></Link>
+              </Badge>
+            </IconButton>
+
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
+};
+
+// class NavBar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+
+//   render() {
+//     return (
+//       <nav className="landing-page-nav-bar">
+//         <div className="landing-page-logo">
+
+//         </div>
+//         <div className="landing-page-search-bar-div">
+//           <input className="landing-page-search-bar-textbox" type="text" src="https://www.kindacode.com/wp-content/uploads/2020/12/search.png" placeholder="Search"></input>
+//         </div>
+//         <div className="landing-page-location-input-div">
+//           <input className="landing-page-location-input-textbox" type="text" placeholder="New York City"></input>
+//         </div>
+//         <div className="landing-page-login-link-div">
+//           <Link to="/sign-in">Sign in</Link>
+//         </div>
+//         <div className="landing-page-cart-link-div">
+//           <Link to="/cart"><ShoppingCartOutlinedIcon /></Link>
+
+//         </div>
+//       </nav>
+//     );
+//   }
+// }
 
 export default NavBar;
