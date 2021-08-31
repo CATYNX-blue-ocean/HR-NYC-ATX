e985d21ba93b97f7fb4fd89cb87bc39f0d5031c6
Polina <chebanenkopolina@gmail.com>
2021-08-31 15:52:45 -0400

    zustand store configured

--- Client/CheckoutPage/tempZustand.js
+++ Client/CheckoutPage/tempZustand.js
@@ -1,8 +1,23 @@
 import create from 'zustand';
+import { exampleData } from '../../exampleData';
 
-const useCheckoutStore = create((set) => ({
-  exampleChangeStateFn: () =>
-    set((state) => ({
-      exampleStateField: state.exampleStateField + 'changed',
-    })),
+const useDataStore = create((set) => ({
+  productData: exampleData.productListings,
+  serviceData: exampleData.serviceListings,
+  currentProduct: exampleData.productListings[0],
+  // this function sets the current product
+  cart: [],
+  setCurrentProduct: (id) =>
+    set((state) => {
+      let current = state.productData.map((product) => {
+        if (product.id === id) {
+          return product;
+        }
+      });
+      return { currentProduct: current };
+    }),
+  // this function adds a product to cart
+  addToCart: (product) => set((state) => ({ cart: product })),
 }));
+
+const products = useDataStore((state) => state.productData);
