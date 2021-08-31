a83610e5616b85073e984c5fb5145415fe038787
Cam Boucher <cam.boucher.28@gmail.com>
2021-08-31 03:03:21 -0400

    adding clickhandler to reroute user back to landing page

--- Client/OrderConfirmation.jsx
+++ Client/OrderConfirmation.jsx
@@ -5,13 +5,19 @@ import { Button } from '@material-ui/core';
 
 
 const OrderConfirmationPage = (props) => {
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    // needs to redirect to landing page use React Router
+  };
+
   return (
     <>
       <NavBar/>
       <img src="https://picsum.photos/200">Placeholder for Shopping Bag Image</img>
       <h3>THANK YOU FOR SHOPPING WITH US</h3>
       <div>Check your email for order confirmation</div>
-      <Button>Shop Again</Button>
+      <Button onClick={handleClick}>Shop Again</Button>
     </>
   );
 };
