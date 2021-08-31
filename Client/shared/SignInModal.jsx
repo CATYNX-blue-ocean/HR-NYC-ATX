c469051a2e0529dfc8d32af5c01758634bb078b2
jdtodd <todddillan@gmail.com>
2021-08-31 14:56:46 -0500

    Validation Completed for Login page

--- Client/shared/SignInModal.jsx
+++ Client/shared/SignInModal.jsx
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Backdrop from '@material-ui/core/Backdrop';
 import Button from '@material-ui/core/Button';
+import validator from 'validator';
 import {
   Link,
   useHistory
@@ -12,13 +13,45 @@ import {
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
   const history = useHistory();
 
   let close = (event) => {
     history.goBack();
   };
 
-  // ^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$
+  const validateEmail = (email) => {
+    let emailRes = false;
+    if (email !== '') {
+      emailRes = !validator.isEmail(email);
+      setEmailError(emailRes);
+    }
+  };
+  const handleSetEmail = (event) => {
+    setEmail(event.target.value);
+    validateEmail(event.target.value);
+  };
+
+  const validatePassword = (password) => {
+    let passLengthValid = false;
+    let passCharsValid = false;
+    if (password !== '') {
+      passLengthValid = !validator.isLength(password, {min: 8, max: 14});
+    }
+    if (password !== '') {
+      passCharsValid = !validator.isAlphanumeric(password);
+    }
+    if (passLengthValid || passCharsValid) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+  };
+  const handleSetPassword = (event) => {
+    setPassword(event.target.value);
+    validatePassword(event.target.value);
+  };
 
   return (
     <div>
@@ -36,10 +69,10 @@ const SignIn = () => {
             </p>
           </Grid>
           <Grid item xs={12}>
-            <TextField required id='sign-in-email' type='email' label='email' onChange={(event) => { setEmail(event.target.value); }}/>
+            <TextField required id='sign-in-email' type='email' label='email' onChange={(event) => { handleSetEmail(event); }} error={emailError}/>
           </Grid>
           <Grid item xs={12}>
-            <TextField required id='sign-in-password' type='password' label='password' onChange={(event) => { setPassword(event.target.value); }}/>
+            <TextField required id='sign-in-password' type='password' label='password' onChange={(event) => { handleSetPassword(event); }} error={passwordError}/>
           </Grid>
           <Grid item xs={12}>
             <Button variant='contained'>Sign In</Button>
