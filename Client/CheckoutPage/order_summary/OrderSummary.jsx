3065b93652ab2cb063b745f9787f37a48c96cac0
Polina <chebanenkopolina@gmail.com>
2021-08-31 17:19:17 -0400

    Order summary started

--- Client/CheckoutPage/order_summary/OrderSummary.jsx
+++ Client/CheckoutPage/order_summary/OrderSummary.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import useDataStore from '../../zustandStore.js';
 
 const OrderSummary = () => {
-  const [cartItems, setCart] = useState([]);
+  // const [cartItems, setCart] = useState([]);
+  const cart = useDataStore((state) => state.cart);
+  console.log(cart);
+  let itemsPrice = 0;
+  cart.map((item) => {
+    itemsPrice += Number(item.price);
+  });
+
   return (
     <div>
       <Grid container spacing={3} style={{ padding: '25px' }}>
         <Grid item xs={12}>
           <h3>Order Summary</h3>
-          <h3>{cartItems.length} Items</h3>
+          <h3>{cart.length} Items</h3>
         </Grid>
         <Grid item xs={6}>
-          <p>names</p>
+          <p>Items:</p>
+          <p>Shipping:</p>
+          <p>Total before tax:</p>
+          <p>Estimated tax:</p>
         </Grid>
         <Grid item xs={6}>
-          <p>prices </p>
+          <p>${itemsPrice}</p>
+          <p>$2.99</p>
+          <p>${itemsPrice + 2.99}</p>
+          <p>${itemsPrice + 2.99}</p>
         </Grid>
       </Grid>
     </div>
