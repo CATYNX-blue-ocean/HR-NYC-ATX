579ccdfd04aa09c39d8d9482161bba3a77806011
Yuki Li <yunqili1025@gmail.com>
2021-08-31 13:52:52 -0400

    add image thumbs

--- Client/ProductDetails/PD-Images.jsx
+++ Client/ProductDetails/PD-Images.jsx
@@ -2,9 +2,23 @@ import React, { useState } from 'react';
 
 const Images = ({ images}) => {
   const [currentImg, setCurrentImg] = useState(images[0]);
-
+  const [thumbs, setThumbs] = useState(images.slice(1));
+  const thumbStyle = {margin: '10px', height: '30px', width: '30px'};
   return (
-    <img src={currentImg} />
+    <div>
+      <div className="main-image">
+        <img src={currentImg} />
+      </div>
+      <div className='thumb-images'>
+        {thumbs.map((thumb, i) => {
+          return (
+            <span className='thumb' key={i} >
+              <img src={thumb} style={thumbStyle}/>
+            </span>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
