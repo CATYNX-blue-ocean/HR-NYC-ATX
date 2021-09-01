0024380b231e1263bd92b287e65ee3de77196dcd
Cam Boucher <cam.boucher.28@gmail.com>
2021-09-01 08:34:26 -0400

    conditional rendering for empty cart view

--- Client/CheckoutPage/cart/Cart.jsx
+++ Client/CheckoutPage/cart/Cart.jsx
@@ -3,9 +3,12 @@ import CartList from './CartList.jsx';
 import OrderSummary from '../order_summary/OrderSummary.jsx';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-// import { cart } from '../zustandStore.js';
+import useDataStore from '../../zustandStore.js';
+import EmptyCart from '../EmptyCart.jsx';
 
 const Cart = () => {
+
+  const cart = useDataStore((state) => state.cart);
 
   return (
 
@@ -14,34 +17,38 @@ const Cart = () => {
         <h2>Your Cart</h2>
       </Grid>
 
-      <Grid container spacing={2}>
-        <Grid
-          item
-          xs={7}
-          style={{
-            border: '1px solid #5E2EBA',
-            boxSizing: 'border-box',
-            borderRadius: '4px',
-            padding: '0px',
-          }}
-        >
-          <CartList />
-        </Grid>
+      {cart.length &&
+        <Grid container spacing={2}>
+          <Grid
+            item
+            xs={7}
+            style={{
+              border: '1px solid #5E2EBA',
+              boxSizing: 'border-box',
+              borderRadius: '4px',
+              padding: '0px',
+            }}
+          >
+            <CartList />
+          </Grid>
 
-        <Grid
-          item
-          xs={5}
-          style={{
-            border: '1px solid #5E2EBA',
-            boxSizing: 'border-box',
-            borderRadius: '4px',
-            padding: '0px',
-          }}
-        >
-          <OrderSummary />
+          <Grid
+            item
+            xs={5}
+            style={{
+              border: '1px solid #5E2EBA',
+              boxSizing: 'border-box',
+              borderRadius: '4px',
+              padding: '0px',
+            }}
+          >
+            <OrderSummary />
+          </Grid>
         </Grid>
-
-      </Grid>
+      }
+      {!cart.length &&
+        <EmptyCart />
+      }
     </Container>
 
   );
