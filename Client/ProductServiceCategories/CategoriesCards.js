a144f81477a4b184542090922ac4612cb705bcb2
swizzlestix09 <angely.rodriguez@gmail.com>
2021-09-01 15:28:54 -0400

    skeleton pages initialized

---
+++ Client/ProductServiceCategories/CategoriesCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+
+const CategoriesCards = (props) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default CategoriesCards;
---
+++ Client/ProductServiceCategories/ProdServCategories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Container } from '@material-ui/core';
+
+const ProdServCategories = (props) => {
+  return (
+    <Container>
+      <div className="ProdServCategories">
+
+      </div>
+    </Container>
+  );
+};
+
+export default ProdServCategories;
