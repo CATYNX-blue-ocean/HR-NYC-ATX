582df5423a0ae959b0e144f7b345d3057dfb9d03
Cam Boucher <cam.boucher.28@gmail.com>
2021-09-01 03:11:41 -0400

    added cart static image

---
+++ Client/CheckoutPage/OrderConfirmationPage.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import NavBar from '../shared/NavBar.jsx';
+import useStyles from '../shared/styles.js';
+import { Button } from '@material-ui/core';
+import happyCart from './happy_cart.png';
+
+//this will correspond to '/orderpost' endpoint
+const OrderConfirmationPage = (props) => {
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    // needs to redirect to landing page use React Router
+  };
+
+  return (
+    <>
+      <NavBar/>
+      <img src="./happy_cart.png">Placeholder for Shopping Bag Image</img>
+      <h3>THANK YOU FOR SHOPPING WITH US</h3>
+      <div>Check your email for order confirmation</div>
+      <Button onClick={handleClick}>Shop Again</Button>
+    </>
+  );
+};
+
+export default OrderConfirmationPage;
