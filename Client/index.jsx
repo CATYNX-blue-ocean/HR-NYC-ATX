3742b48db2f5b4f26dce8d5534f4dce5e512a1ed
Jason <jasondye.music@gmail.com>
2021-09-02 09:21:45 -0500

    updating from dev

--- Client/index.jsx
+++ Client/index.jsx
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-import Cart from './CheckoutPage/cart/Cart.jsx';
-
-// ReactDOM.render(<App />, document.getElementById('app'));
-ReactDOM.render(<Cart />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
