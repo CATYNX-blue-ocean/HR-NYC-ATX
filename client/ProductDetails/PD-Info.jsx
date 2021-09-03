f7394366a953fb91a9e40ec39383b32825ef124e
swizzlestix09 <angely.rodriguez@gmail.com>
2021-09-03 15:10:03 -0400

    padding added to text & stars in product info

--- client/ProductDetails/PD-Info.jsx
+++ client/ProductDetails/PD-Info.jsx
@@ -12,20 +12,20 @@ const Info = ({ name, price, description, ratings }) => {
   var avgRating = Number(avgRating);
 
   return (
-    <Container maxWidth="sm" styles={{ float: 'left' }}>
+    <Container maxWidth="sm" styles={{ float: 'left'}}>
       <Typography
         variant="h5"
         color="textSecondary"
         component="p"
-        style={{ fontWeight: 'bold', color: 'black' }}
+        style={{ fontWeight: 'bold', color: 'black', paddingBottom: '2vh' }}
       >
         {name}
       </Typography>
-      <Typography variant="h6" color="black" component="p">
+      <Typography variant="h6" color="black" component="p" style={{ paddingBottom: '1vh' }}>
         ${price} USD
         <br />
         <Typography variant="body1" color="black" component="p">
-          <span style={{ flexDirection: 'row', width: '50%' }}>
+          <span style={{ flexDirection: 'row', width: '50%', paddingBottom: '1vh'}}>
             {description}
           </span>
         </Typography>
@@ -33,7 +33,7 @@ const Info = ({ name, price, description, ratings }) => {
       <Typography variant="body1" color="black" component="p">
         {avgRating}
         <Rating
-          style={{ color: '#5E2EBA', paddingLeft: '5%', position: 'relative' }}
+          style={{ color: '#5E2EBA', paddingLeft: '5%', position: 'relative', paddingBottom: '2vh' }}
           name="half-rating-read"
           defaultValue={avgRating}
           precision={0.25}
